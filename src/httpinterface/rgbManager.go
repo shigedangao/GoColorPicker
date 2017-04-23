49d8b9e00c23aff2a2bec9dbd3be6688bb0da25c
Marc Intha-amnouay <marc.inthaamnouay@gmail.com>
2017-04-23 22:59:56 +0200

    cleanup the git

--- src/httpinterface/rgbManager.go
+++
@@ -1,7 +0,0 @@
-package serverManager
-
-// getRgbResults will return the data or the error to send back to the http req
-func (c ColorHttpHandler) getRgbResults() ([]byte, error) {
-
-	return []byte(""), nil
-}
